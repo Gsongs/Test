e9ccd745e173c1eca2c8d62794500401fd91cb86
=33 <=www.1453126078@qq.com>
2021-11-10 13:19:34 +0800

    first commit

---
+++ app/src/main/java/com/bawei/wang/String.java
@@ -0,0 +1,13 @@
+package com.bawei.wang;
+
+public
+/**
+ *作者：王先生
+ *时间：2021/11/10 11:22
+ *用途：
+ */
+class String {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
